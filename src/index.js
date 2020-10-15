fa0e016dd8f52edd950e2d650b107c3480122b2b
Daniil Denisov <69167872+L1berkin@users.noreply.github.com>
2020-10-15 16:15:44 +0300

    Framework start (#3)
    * Finish base framework functional
    
    * Comment debag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import {Excel} from '@/components/excel/Excel'
+import {Formula} from './components/formula/Formula';
+import {Header} from './components/header/Header';
+import {Table} from './components/table/Table';
+import {Toolbar} from './components/toolbar/Toolbar';
 import './scss/index.scss'
 
-console.log('Working!')
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
 
-// Last lesson = 17
+excel.render()
+// 34
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ const jsLoaders = () => {
   const loaders = [{
     loader: 'babel-loader',
     options: {
-      presets: ['@babel/preset-env']
+      presets: ['@babel/preset-env'],
+      plugins: ['@babel/plugin-proposal-class-properties']
     }
   }]
 
